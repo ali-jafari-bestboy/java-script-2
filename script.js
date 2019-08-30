7985863a0ad97e62be73bef147b7a03e96f8d8ae
ali <ali-jafari-bestboy@gmail.com>
2019-08-30 05:03:36 -0700

    The week 2 homework

---
+++ script.js
@@ -0,0 +1,74 @@
+//1
+let hi = {English:"Hello World", Farsi:"SALAM JAHAN", Greek:"Γειά σου Κόσμε"};
+console.log(hi)
+//2
+console.log("I'm awesome");
+//3
+let x;
+console.log(x)
+//3.1
+x = 6;
+console.log('the value of my variable x will be:6')
+console.log(x)
+//4
+
+const y='jafari';
+console.log('that is my family name');
+console.log(y);
+//5
+//i ahave to write in this place for number 5
+//6
+myarray =[];
+console.log(myarray);
+console.log("My array");
+console.log(myarray);
+let  animal = ["dog", "cat", "unt", "kangoro"];
+console.log(animal.push("baby pig"));
+console.log(animal);
+//7
+let myString = "this is a test"
+console.log(myString.length);
+console.log(myString );
+console.log(myString.length)
+// 8
+const h = 2;
+const name ="ali jafari";
+const gamer =10;
+const  best =100;
+
+console.log(h);
+console.log(name);
+console.log(gamer);
+console.log(best);
+console.log(typeof h);
+console.log(typeof name);
+console.log(typeof gamer);
+console.log(typeof true);
+
+(typeof h==typeof name) ? console.log('same type'):console.log('no');
+
+(typeof h==typeof gamer) ? console.log('same type'):console.log('no');
+
+(typeof h==typeof best) ? console.log('same type'):console.log('no');
+
+(typeof name==typeof gamer) ? console.log('same type'):console.log('no');
+
+(typeof name==typeof best) ? console.log('same type'):console.log('no');
+
+(typeof best==typeof gamer) ? console.log('same type'):console.log('no');
+// 9
+var i =7;
+console.log(i%3);
+console.log("that mean remainder in our division how many it is ");
+console.log("and that is a easy way to understand how much remainer we have");
+console.log("if you want to know how much remainder you can use it");
+//10
+let myarray1=["afghanistan",1,3,45,true ,undefined,null,]
+console.log('this works'+ myarray1);
+if (6/0 === 10/0){
+    console.log("yes")
+}else{
+    console.log("no")
+}
+
+
